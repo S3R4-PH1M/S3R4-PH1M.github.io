b066e9bf3671ac9b510e78f53900f4a31001d7cb
S3R4-PH1M <rayne@karma.co.nz>
2026-08-15 19:01:17 +1000

    Created main.js file and removed inline JavaScript from index.html. Also centralised the Nav element CSS styling into a single class in index.css

---
+++ Resources/Scripts/main.js
@@ -0,0 +1,23 @@
+var accordions = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+function myFunction() {
+  var x = document.getElementById("mobileNav");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+} 
